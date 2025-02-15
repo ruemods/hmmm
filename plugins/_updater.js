731808da6fc8e719b5be1e80aa1d631ccbc511c9
KichuExe <99080094+KichuExe@users.noreply.github.com>
2025-02-15 18:37:51 +0530

    fixed?

---
+++ plugins/_updater.js
@@ -0,0 +1,22 @@
+const {Spakry} = require('../lib');
+const simpleGit = require('simple-git');
+const git = simpleGit();
+
+Sparky({
+      name: "update",
+      fromMe: true,
+      desc: "Update",
+      category: "app",
+  },
+  async ( {
+        m
+  }) => {
+await git.fetch();
+var commits = await git.log([
+        'main' + "..origin/" + 'main',
+      ]);
+let message = "*_New updates available!_*\n\n";
+commits["all"].map((e, i) =>
+message += "```" + `${i + 1}.▪️[${e.date.substring(0, 10)}]:<${e.message}>\n` + "```")
+return await m.reply(commits.total !== 0 ? message : "```Bot is up-to-date!```");
+ });
