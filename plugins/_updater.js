5d8e1df3a0e892d685b38dec90c9fba8208071a9
KichuExe <99080094+KichuExe@users.noreply.github.com>
2025-02-16 17:16:22 +0530

    testing..

--- plugins/_updater.js
+++ plugins/_updater.js
@@ -1,4 +1,5 @@
 const {Sparky} = require('../lib');
+const {updateApp} = require('./pluginsCore');
 const simpleGit = require('simple-git');
 const git = simpleGit();
 
@@ -9,6 +10,7 @@ Sparky({
       category: "app",
   },
   async ( {
+        args,
         m
   }) => {
 await git.fetch();
@@ -19,4 +21,15 @@ let message = "*_New updates available!_*\n\n";
 commits["all"].map((e, i) =>
 message += "```" + `${i + 1}. ${e.message}\n[${e.date.substring(0, 10)}]\n` + "```")
 return await m.reply(commits.total !== 0 ? message + `\n_Use '${m.prefix}update now' to update the bot._` : "```Bot is up-to-date!```");
+        if(args && (args.toLowerCase() === 'now')) {
+              await git.fetch();
+              const branches = await git.branch(['-r']);
+		const remoteBranch = branches.all.find(branch => branch.includes('origin/main'));
+		if (!remoteBranch) return;
+              const commit = await git.log([`HEAD..${remoteBranch}`]);
+		if (!commit.total > 0) {
+			return await m.reply("```Bot is up-to-date!```");
+            }
+              await updateApp(message);
+        }
  });
